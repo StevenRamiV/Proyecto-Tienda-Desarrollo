82d4636275341cac3ea8a155c179c940524c20a4
AMD <AMD@DESKTOP-THRJ96D>
2023-06-25 23:58:03 -0600

    Creación del proyecto inicial

---
+++ src/main/java/com/Tienda/domain/Categoria.java
@@ -0,0 +1,35 @@
+
+package com.Tienda.domain;
+
+import jakarta.persistence.*;
+import java.io.Serializable;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="categoria")
+public class Categoria implements Serializable {
+    
+    // VERSION DE SERIALIZACION
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY)
+    private Long idCategoria; // Se transforma en id_categoria
+    private String descripcion;
+    private String rutaimagen;
+    private boolean activo;
+
+    public Categoria() {
+    }
+
+    public Categoria(String descripcion, String rutaimagen, boolean activo) {
+        this.descripcion = descripcion;
+        this.rutaimagen = rutaimagen;
+        this.activo = activo;
+    }
+
+    public boolean isActivo() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+}
---
+++ src/main/java/com/Tienda/service/CategoriaService.java
@@ -0,0 +1,13 @@
+
+package com.Tienda.service;
+
+import com.Tienda.domain.Categoria;
+import java.util.List;
+
+
+public interface CategoriaService {
+    
+    // Metodo que retorna la lista de categorias
+    public List<Categoria> getCategorias(boolean activos);
+    
+}
