3efae893e491a284df3f7c76a0cb958e4823e7d0
AMD <AMD@DESKTOP-THRJ96D>
2023-08-01 09:44:08 -0600

    Commit Semana 11

--- src/main/java/com/Tienda/dao/UsuarioDao.java
+++ src/main/java/com/Tienda/dao/UsuarioDao.java
@@ -1,13 +1,15 @@
 
-package com.Tienda.dao;
+package com.tienda.dao;
 
 import com.Tienda.domain.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-
-
-public interface UsuarioDao extends JpaRepository<Usuario, Long>{
-    
+public interface UsuarioDao extends JpaRepository<Usuario, Long> {
     Usuario findByUsername(String username);
     
+    Usuario findByUsernameAndPassword(String username, String Password);
+
+    Usuario findByUsernameOrCorreo(String username, String correo);
+
+    boolean existsByUsernameOrCorreo(String username, String correo);
 }
